637ad390663a0db36f3160eba25d3746fd36c252
unknown <voldam@ lolamgit config --global user.email volidam>
2024-04-08 15:03:38 +0300

    it was my homework

---
+++ main.js
@@ -0,0 +1,41 @@
+// // // let person = {
+// // //     name: "Muhammadjon",
+// // //     surname: "Dilshodov",
+// // //     age: "9",
+// // //     rang
+// // // }
+
+
+// // let computer = {
+// //     proters: "proters",
+// //     ekran: "ekran",
+// //     klavaturia: "klavaturia", 
+// //     mishka: "mishka",
+
+// //     hobbiy: person = {
+// //         play: "tens",
+// //         nose: "high"
+// //     }
+// // }
+// let car ={
+//     rang: "qora",
+//     bakovoy: "bakovoy",
+
+// }
+// console.log(car);
+// console.log(car):
+
+
+
+
+const array = prompt("Ismingizni kiriting");
+const array2 = prompt("Space ID yingizni kiriting");
+const array3 = prompt("Space passwordingizni kiritng");
+
+let person = {
+    name: array,
+    id: array2,
+    password: array3,
+}
+
+console.log(person);
